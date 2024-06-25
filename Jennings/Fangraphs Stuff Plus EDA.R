dec9271322c1b05a342b0141ff27d25f460c2cfa
Liam Jennings <liam.j.jennings12@gmail.com>
2024-06-25 09:42:02 -0400

    Added PitchingBot variables with an initial table and graph comparing the averages to Stuff+

--- Jennings/Fangraphs Stuff Plus EDA.R
+++ Jennings/Fangraphs Stuff Plus EDA.R
@@ -12,11 +12,25 @@ fg_stuff_plus_20 <- baseballr::fg_pitcher_leaders(startseason = "2020", endseaso
 ##### select columns
 fg_stuff_plus_20 <- fg_stuff_plus_20 |> 
   select(Season, Throws, PlayerName, IP, Relief_IP,
+         # stuff plus stuff variables
          stuff_plus_CH = sp_s_CH, stuff_plus_FF = sp_s_FF, 
          stuff_plus_SL = sp_s_SL, stuff_plus_KC = sp_s_KC,
          stuff_plus_FC = sp_s_FC, stuff_plus_CU = sp_s_CU, 
          stuff_plus_SI = sp_s_SI, stuff_plus_FS = sp_s_FS,
-         stuff_plus = sp_stuff) |> 
+         stuff_plus = sp_stuff,
+         # stuff plus location variables
+         stuff_plus_loc_CH = sp_l_CH, stuff_plus_loc_FF = sp_l_FF, 
+         stuff_plus_loc_SL = sp_l_SL, stuff_plus_loc_KC = sp_l_KC,
+         stuff_plus_loc_FC = sp_l_FC, stuff_plus_loc_CU = sp_l_CU, 
+         stuff_plus_loc_SI = sp_l_SI, stuff_plus_loc_FS = sp_l_FS,
+         stuff_plus_loc = sp_location,
+         # PitchingBot stuff
+         pb_s_CH, pb_s_FF, 
+         pb_s_SL, pb_s_KC,
+         pb_s_FC, pb_s_CU, 
+         pb_s_SI, pb_s_FS,
+         pb_stuff
+         ) |> 
   filter(IP >= 6) |> 
   mutate(Role = ifelse(Relief_IP <= IP * 0.25 | is.na(Relief_IP) == TRUE, "SP", "RP"))
 
@@ -30,7 +44,18 @@ fg_stuff_plus_21 <- fg_stuff_plus_21 |>
          stuff_plus_SL = sp_s_SL, stuff_plus_KC = sp_s_KC,
          stuff_plus_FC = sp_s_FC, stuff_plus_CU = sp_s_CU, 
          stuff_plus_SI = sp_s_SI, stuff_plus_FS = sp_s_FS,
-         stuff_plus = sp_stuff) |> 
+         stuff_plus = sp_stuff,
+         stuff_plus_loc_CH = sp_l_CH, stuff_plus_loc_FF = sp_l_FF, 
+         stuff_plus_loc_SL = sp_l_SL, stuff_plus_loc_KC = sp_l_KC,
+         stuff_plus_loc_FC = sp_l_FC, stuff_plus_loc_CU = sp_l_CU, 
+         stuff_plus_loc_SI = sp_l_SI, stuff_plus_loc_FS = sp_l_FS,
+         stuff_plus_loc = sp_location,
+         # PitchingBot stuff
+         pb_s_CH, pb_s_FF, 
+         pb_s_SL, pb_s_KC,
+         pb_s_FC, pb_s_CU, 
+         pb_s_SI, pb_s_FS,
+         pb_stuff) |> 
   filter(IP >= 9) |> 
   mutate(Role = ifelse(Relief_IP <= IP * 0.25 | is.na(Relief_IP) == TRUE, "SP", "RP"))
 
@@ -44,7 +69,18 @@ fg_stuff_plus_22 <- fg_stuff_plus_22 |>
          stuff_plus_SL = sp_s_SL, stuff_plus_KC = sp_s_KC,
          stuff_plus_FC = sp_s_FC, stuff_plus_CU = sp_s_CU, 
          stuff_plus_SI = sp_s_SI, stuff_plus_FS = sp_s_FS,
-         stuff_plus = sp_stuff) |> 
+         stuff_plus = sp_stuff,
+         stuff_plus_loc_CH = sp_l_CH, stuff_plus_loc_FF = sp_l_FF, 
+         stuff_plus_loc_SL = sp_l_SL, stuff_plus_loc_KC = sp_l_KC,
+         stuff_plus_loc_FC = sp_l_FC, stuff_plus_loc_CU = sp_l_CU, 
+         stuff_plus_loc_SI = sp_l_SI, stuff_plus_loc_FS = sp_l_FS,
+         stuff_plus_loc = sp_location,
+         # PitchingBot stuff
+         pb_s_CH, pb_s_FF, 
+         pb_s_SL, pb_s_KC,
+         pb_s_FC, pb_s_CU, 
+         pb_s_SI, pb_s_FS,
+         pb_stuff) |> 
   filter(IP >= 9) |> 
   mutate(Role = ifelse(Relief_IP <= IP * 0.25 | is.na(Relief_IP) == TRUE, "SP", "RP"))
 
@@ -58,7 +94,18 @@ fg_stuff_plus_23 <- fg_stuff_plus_23 |>
          stuff_plus_SL = sp_s_SL, stuff_plus_KC = sp_s_KC,
          stuff_plus_FC = sp_s_FC, stuff_plus_CU = sp_s_CU, 
          stuff_plus_SI = sp_s_SI, stuff_plus_FS = sp_s_FS,
-         stuff_plus = sp_stuff) |> 
+         stuff_plus = sp_stuff,
+         stuff_plus_loc_CH = sp_l_CH, stuff_plus_loc_FF = sp_l_FF, 
+         stuff_plus_loc_SL = sp_l_SL, stuff_plus_loc_KC = sp_l_KC,
+         stuff_plus_loc_FC = sp_l_FC, stuff_plus_loc_CU = sp_l_CU, 
+         stuff_plus_loc_SI = sp_l_SI, stuff_plus_loc_FS = sp_l_FS,
+         stuff_plus_loc = sp_location,
+         # PitchingBot stuff
+         pb_s_CH, pb_s_FF, 
+         pb_s_SL, pb_s_KC,
+         pb_s_FC, pb_s_CU, 
+         pb_s_SI, pb_s_FS,
+         pb_stuff) |> 
   filter(IP >= 10.5) |> 
   mutate(Role = ifelse(Relief_IP <= IP * 0.25 | is.na(Relief_IP) == TRUE, "SP", "RP")) 
 
@@ -73,7 +120,18 @@ fg_stuff_plus_24 <- fg_stuff_plus_24 |>
          stuff_plus_SL = sp_s_SL, stuff_plus_KC = sp_s_KC,
          stuff_plus_FC = sp_s_FC, stuff_plus_CU = sp_s_CU, 
          stuff_plus_SI = sp_s_SI, stuff_plus_FS = sp_s_FS,
-         stuff_plus = sp_stuff) |> 
+         stuff_plus = sp_stuff,
+         stuff_plus_loc_CH = sp_l_CH, stuff_plus_loc_FF = sp_l_FF, 
+         stuff_plus_loc_SL = sp_l_SL, stuff_plus_loc_KC = sp_l_KC,
+         stuff_plus_loc_FC = sp_l_FC, stuff_plus_loc_CU = sp_l_CU, 
+         stuff_plus_loc_SI = sp_l_SI, stuff_plus_loc_FS = sp_l_FS,
+         stuff_plus_loc = sp_location,
+         # PitchingBot stuff
+         pb_s_CH, pb_s_FF, 
+         pb_s_SL, pb_s_KC,
+         pb_s_FC, pb_s_CU, 
+         pb_s_SI, pb_s_FS,
+         pb_stuff) |> 
   filter(IP >= 9) |> 
   mutate(Role = ifelse(Relief_IP <= IP * 0.25 | is.na(Relief_IP) == TRUE, "SP", "RP"))
 
@@ -198,7 +256,8 @@ fg_stuff_plus |>
 
 ## mean Stuff+ for each pitch
 fg_stuff_plus |> 
-  select(stuff_plus_CH:stuff_plus) |> 
+  select(stuff_plus_CH:stuff_plus,
+         pb_s_CH:pb_stuff) |> 
   map(mean, na.rm = TRUE) |> 
   unlist()
 
@@ -207,6 +266,24 @@ fg_stuff_plus |>
 #### LOOK AT PAUL SKENES RUN VALUE FOR SPLITTER AND SLIDER 
 ##### RUN VALUE SLIDER: -2
 ##### RUN VALUE SPLITTER: 10
+
+fg_stuff_plus |> 
+  mutate(Season_fct = factor(Season)) |> 
+  ggplot(aes(x = pb_s_FS, y = Season_fct, fill = Season_fct)) +
+  geom_density_ridges() +
+  labs(
+    x = "PitchingBot Stuff",
+    y = "Season",
+    title = "PitchingBot Stuff Splitter from 2020-24"
+  ) +
+  geom_vline(xintercept = mean(fg_stuff_plus$pb_s_FS, na.rm = TRUE),
+             color = "firebrick1", linetype = "dashed", linewidth = 1.5) +
+  scale_fill_colorblind() +
+  theme_bw() +
+  theme(
+    legend.position = "none",
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 20)
+  )
 
 ##### NEXT: USE STATCAST TO CALCULATE RUN VALUE AND COMPARE TO STUFF+
 ##### ALSO, COMPARE STUFF+ TO PITCH+, LOCATION+, AND PITCHINGBOT
