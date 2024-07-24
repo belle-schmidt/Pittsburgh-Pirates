bd78493fe842df736830605753b99ccc9bbd53bc
Liam Jennings <liam.j.jennings12@gmail.com>
2024-07-23 23:21:35 -0400

    Added xwOBA GAM model to compare with Random Forest

--- Jennings/GAM_Stuff_Plus_Model.R
+++ Jennings/GAM_Stuff_Plus_Model.R
@@ -89,3 +89,74 @@ stuff_plus_gam |>
   augment() |> 
   mutate(pred = round(.fitted)) |> 
   summarize(rmse = sqrt(mean((stuff_plus - .fitted)^2)))
+
+
+
+
+## Fitting GAM with mgcv for xwOBA
+### Training data
+set.seed(0829)
+train <- ff_models |> 
+  slice_sample(prop = 0.7)
+test <- ff_models |> 
+  anti_join(train)
+
+### Fit model
+stuff_plus_gam <- gam(xwOBA ~ s(extension) + s(horizontal_break) + 
+                        s(induced_vertical_break) + s(velocity) + s(spin),
+                      family = "gaussian",
+                      method = "REML", # more stable solution than default
+                      data = train)
+
+### View model summary
+#### Splines
+stuff_plus_gam |> 
+  tidy()
+
+#### Parametric
+stuff_plus_gam |> 
+  tidy(parametric = TRUE)
+
+#### Model G-o-F (emphasize sampling)
+stuff_plus_gam |> 
+  glance()
+
+## Visualize partial response functions
+#install.packages('gratia')
+draw(stuff_plus_gam)
+
+### for model diagnostics
+stuff_plus_gam |> 
+  appraise()
+
+### Model checking
+stuff_plus_gam |> 
+  gam.check()
+
+## Evaluate prediction accuracy
+### In-sample performance (training set)
+stuff_plus_gam |> 
+  augment(newdata = train) |> 
+  mutate(pred = round(.fitted)) |> 
+  summarize(rmse = sqrt(mean((stuff_plus - .fitted)^2)))
+
+### Out-of-sample performance (test set)
+stuff_plus_gam |> 
+  augment(newdata = test) |> 
+  mutate(pred = round(.fitted)) |> 
+  summarize(rmse = sqrt(mean((stuff_plus - .fitted)^2)))
+
+
+## Final model
+### Fit model
+stuff_plus_gam <- gam(stuff_plus ~ extension + s(horizontal_break) + 
+                        s(induced_vertical_break) + s(velocity) + s(spin),
+                      family = "gaussian",
+                      method = "REML", 
+                      data = pitching_models)
+
+### RMSE
+stuff_plus_gam |> 
+  augment() |> 
+  mutate(pred = round(.fitted)) |> 
+  summarize(rmse = sqrt(mean((stuff_plus - .fitted)^2)))
