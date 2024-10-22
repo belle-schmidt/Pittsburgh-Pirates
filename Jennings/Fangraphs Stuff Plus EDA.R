d3d3eec464ee1b70e020e2c772cc8957bad5dfcc
Liam Jennings <liam.j.jennings12@gmail.com>
2024-10-21 23:19:09 -0400

    Removed 2020 data (COVID season) and updated data to include 2024

--- Jennings/Fangraphs Stuff Plus EDA.R
+++ Jennings/Fangraphs Stuff Plus EDA.R
@@ -10,80 +10,11 @@ library(scales)
 #install.packages('gtExtras')
 library(gtExtras)
 
-
-
-# NOTES -------------------------------------------------------------------
-##### ADD NA COLUMNS FOR FORKBALL & KNUCKLEBALL
-
-###### INTERESTING THOUGHT ######
-##### PAUL SKENES SLIDER STUFF IS WELL ABOVE AVERAGE FOR BOTH MODELS
-##### HIS SPLITTER IS ONLY SLIGHTLY ABOVE AVERAGE
-##### SLIDERS HAVE THE HIGHEST AVERAGE STUFF+ STUFF OF THE PITCH TYPES
-#### LOOK AT PAUL SKENES RUN VALUE FOR SPLITTER AND SLIDER 
-##### RUN VALUE SLIDER: -2
-##### RUN VALUE SPLITTER: 10
-
-
-##### NEXT: USE STATCAST TO CALCULATE RUN VALUE AND COMPARE TO STUFF+
-##### ALSO, COMPARE STUFF+ TO PITCH+, LOCATION+, AND PITCHINGBOT
-##### LOOK INTO DIFFERENCES BETWEEN ROLES
-
 # PROJECT -----------------------------------------------------------------
 
 
 
 ### Data
-#### 2020
-fg_stuff_plus_20 <- baseballr::fg_pitcher_leaders(startseason = "2020", endseason = "2020")
-
-##### select columns
-fg_stuff_plus_20 <- fg_stuff_plus_20 |> 
-  select(Season, Throws, PlayerName, xMLBAMID, IP, Relief_IP,
-         # stuff plus stuff variables
-         stuff_plus_stuff_CH = sp_s_CH, stuff_plus_stuff_FF = sp_s_FF, 
-         stuff_plus_stuff_SL = sp_s_SL, stuff_plus_stuff_KC = sp_s_KC,
-         stuff_plus_stuff_FC = sp_s_FC, stuff_plus_stuff_CU = sp_s_CU, 
-         stuff_plus_stuff_SI = sp_s_SI, stuff_plus_stuff_FS = sp_s_FS,
-         stuff_plus_stuff = sp_stuff,
-         # stuff plus location variables
-         stuff_plus_loc_CH = sp_l_CH, stuff_plus_loc_FF = sp_l_FF, 
-         stuff_plus_loc_SL = sp_l_SL, stuff_plus_loc_KC = sp_l_KC,
-         stuff_plus_loc_FC = sp_l_FC, stuff_plus_loc_CU = sp_l_CU, 
-         stuff_plus_loc_SI = sp_l_SI, stuff_plus_loc_FS = sp_l_FS,
-         stuff_plus_loc = sp_location,
-         # stuff plus pitch variables
-         stuff_plus_pit_CH = sp_l_CH, stuff_plus_pit_FF = sp_l_FF, 
-         stuff_plus_pit_SL = sp_l_SL, stuff_plus_pit_KC = sp_l_KC,
-         stuff_plus_pit_FC = sp_l_FC, stuff_plus_pit_CU = sp_l_CU, 
-         stuff_plus_pit_SI = sp_l_SI, stuff_plus_pit_FS = sp_l_FS,
-         stuff_plus_pit = sp_pitching,
-         # PitchingBot stuff
-         pitch_bot_stuff_CH = pb_s_CH, pitch_bot_stuff_FF = pb_s_FF, 
-         pitch_bot_stuff_SL = pb_s_SL, pitch_bot_stuff_KC = pb_s_KC,
-         pitch_bot_stuff_FC = pb_s_FC, pitch_bot_stuff_CU = pb_s_CU, 
-         pitch_bot_stuff_SI = pb_s_SI, pitch_bot_stuff_FS = pb_s_FS,
-         pitch_bot_stuff = pb_stuff,
-         # PitchingBot command
-         pitch_bot_com_CH = pb_c_CH, pitch_bot_com_FF = pb_c_FF, 
-         pitch_bot_com_SL = pb_c_SL, pitch_bot_com_KC = pb_c_KC,
-         pitch_bot_com_FC = pb_c_FC, pitch_bot_com_CU = pb_c_CU, 
-         pitch_bot_com_SI = pb_c_SI, pitch_bot_com_FS = pb_c_FS,
-         pitch_bot_com = pb_command,
-         # PitchingBot overall
-         pitch_bot_ovr_CH = pb_o_CH, pitch_bot_ovr_FF = pb_o_FF, 
-         pitch_bot_ovr_SL = pb_o_SL, pitch_bot_ovr_KC = pb_o_KC,
-         pitch_bot_ovr_FC = pb_o_FC, pitch_bot_ovr_CU = pb_o_CU, 
-         pitch_bot_ovr_SI = pb_o_SI, pitch_bot_ovr_FS = pb_o_FS,
-         pitch_bot_ovr = pb_overall,
-         # pitch F/X pitch frequencies
-         pfx_CH_pct, pfx_FA_pct,
-         pfx_SL_pct, pfx_KC_pct,
-         pfx_FC_pct, pfx_CU_pct,
-         pfx_SI_pct, pfx_FS_pct
-         ) |> 
-  filter(IP >= 6) |> 
-  mutate(Role = ifelse(Relief_IP <= IP * 0.25 | is.na(Relief_IP) == TRUE, "SP", "RP"))
-
 #### 2021
 fg_stuff_plus_21 <- baseballr::fg_pitcher_leaders(startseason = "2021", endseason = "2021")
 
@@ -313,20 +244,19 @@ fg_stuff_plus_24 <- fg_stuff_plus_24 |>
 
 ##### add NA's in forkball vectors because forkballs were not thrown until 2023
 sp_s_FO <- c(rep(NA, 
-      sum(nrow(fg_stuff_plus_20), nrow(fg_stuff_plus_21), nrow(fg_stuff_plus_22))), 
+      sum(nrow(fg_stuff_plus_21), nrow(fg_stuff_plus_22))), 
   sp_s_FO_23, sp_s_FO_24)
 
 sp_l_FO <- c(rep(NA, 
-                 sum(nrow(fg_stuff_plus_20), nrow(fg_stuff_plus_21), nrow(fg_stuff_plus_22))), 
+                 sum(nrow(fg_stuff_plus_21), nrow(fg_stuff_plus_22))), 
              sp_l_FO_23, sp_l_FO_24)
 
 sp_p_FO <- c(rep(NA, 
-                 sum(nrow(fg_stuff_plus_20), nrow(fg_stuff_plus_21), nrow(fg_stuff_plus_22))), 
+                 sum(nrow(fg_stuff_plus_21), nrow(fg_stuff_plus_22))), 
              sp_p_FO_23, sp_p_FO_24)
 
 #### row bind
 fg_stuff_plus <- rbind(
-  fg_stuff_plus_20,
   fg_stuff_plus_21,
   fg_stuff_plus_22,
   fg_stuff_plus_23,
@@ -341,7 +271,7 @@ fg_stuff_plus$stuff_plus_loc_FO <- sp_l_FO
 fg_stuff_plus$stuff_plus_pit_FO <- sp_p_FO
 
 #### write to dataset
-write.csv(fg_stuff_plus, "Fangraphs_Pitching_Models_2020-24.csv")
+write.csv(fg_stuff_plus, "Fangraphs_Pitching_Models_2021-24.csv")
 
 ### plots
 #### Stuff+ Changeup over years
